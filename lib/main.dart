54001d4af16f714c9c0d1c122748536b6a491c93
SherazAli-67 <soomrosheraz054@gmail.com>
2025-01-02 22:00:40 +0500

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hitch Player Finder',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: SafeArea(
+          child: Center(child: Text("Welcome To Hitch - Player Finder", style:  TextStyle(fontSize: 26),),),
+        ),
+      )
+    );
+  }
+}
